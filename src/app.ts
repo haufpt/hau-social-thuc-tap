138ab9cc0827264b3e74f97cfb11782316fb3fb9
Hau dep trai <haundps26258@fpt.edu.vn>
2024-01-09 23:23:19 +0700

    mongo connection

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,7 @@
 //chay constructor giong nhu 1 get
 import { Route } from "core/interfaces";
 import express from 'express';
+import mongoose from "mongoose";
 class App{
     public app: express.Application;
     public port: string | number;
@@ -10,6 +11,7 @@ class App{
         this.app=express();
         this.port=process.env.PORT || 9999;
         this.initializeRoutes(routes);
+        this.connectToDatabase();
     }
 
   
@@ -25,6 +27,15 @@ class App{
         routes.forEach((route)=>{
             this.app.use('/', route.router)
         });
+    }
+
+    private connectToDatabase(){
+        try {
+            mongoose.connect('mongodb+srv://nguyenduchau12g:projectdautien@main.sbksmxq.mongodb.net/?retryWrites=true&w=majority');
+            console.log("Database connected")
+        } catch (error) {
+            console.log("Connect to Db error")
+        }
     }
 }
 export default App;
