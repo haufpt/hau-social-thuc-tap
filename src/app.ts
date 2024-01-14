be698554a381a74b4f93e5640b5af93a6faed089
Hau dep trai <haundps26258@fpt.edu.vn>
2024-01-14 18:25:31 +0700

    handle error middleware

--- src/app.ts
+++ src/app.ts
@@ -3,22 +3,49 @@
 import { Route } from "@core/interfaces";
 import express from 'express';
 import mongoose from "mongoose";
+import hpp from 'hpp'
+import morgan from 'morgan'
+import cors from 'cors'
+import helmet from 'helmet'
+import { Logger } from "@core/utils";
+import { errorMiddleware } from "@core/middleware";
 class App{
     public app: express.Application;
     public port: string | number;
+    public production: boolean;
 
     constructor(routes: Route[]){
         this.app=express();
         this.port=process.env.PORT || 9999;
+        this.production=process.env.NODE_ENV=="production"?true:false;
         this.initializeRoutes(routes);
         this.connectToDatabase();
+        this.initializeMiddlewares();
     }
+
+
+private initializeMiddlewares(){
+    if(this.production){
+        this.app.use(hpp());
+        this.app.use(helmet());
+        this.app.use(morgan('combined'));
+        this.app.use(cors({origin: 'your.domain.com', credentials: true}));
+
+
+    }else{
+        this.app.use(morgan('dev'));
+        this.app.use(cors({origin: true, credentials: true}));
+
+    }
+
+    this.app.use(errorMiddleware);
+}
 
   
 
     public listen(){
        this.app.listen(this.port, ()=>{
-            console.log(`Server is listening on port ${this.port}`);
+            Logger.info(`Server is listening on port ${this.port}`);
         });
     }
 
@@ -33,13 +60,13 @@ class App{
         const connectString=process.env.MONGODB_URL;
 
      if(!connectString){
-            console.log("Connection string is invalid");
+        Logger.error("Connection string is invalid");
         }
         try {
             mongoose.connect(`${connectString}`);
-            console.log("Database connected")
+            Logger.info("Database connected")
         } catch (error) {
-            console.log("Connect to Db error")
+            Logger.error("Connect to Db error")
         }
     }
 }
